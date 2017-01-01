ec429255ace450a8be479511dae14e72b239077b
KohheiAdachi <d13301@yonago.kosen-ac.jp>
2017-01-02 01:09:09 +0900

    Initial Commit

---
+++ raspiview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raspiview
+//
+//  Created by 安達康平 on 2017/01/02.
+//  Copyright © 2017年 Kohei Adachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
