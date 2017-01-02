5c5dc83e21c57f110a44579ac6d6ea9f8ed2cfd5
KohheiAdachi <d13301@yonago.kosen-ac.jp>
2017-01-03 01:56:58 +0900

    webviewで映像表示

--- raspiview/ViewController.swift
+++ raspiview/ViewController.swift
@@ -12,12 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCamView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func start(_ sender: Any) {
+     loadCamView()
+}
+
+    @IBOutlet weak var monitor: UIWebView!
+    
+    func loadCamView(){
+    
+        let url: String = "http://192.168.1.43:8081/javascript_simple.html"
+        let requestURL = NSURL(string: url)
+        let req = NSURLRequest(url: requestURL! as URL)
+        
+        monitor.loadRequest(req as URLRequest)
     }
 
 
